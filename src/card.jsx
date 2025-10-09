f5f9e5e762518d1d96c37edd4299530346e9ce92
aryaman5372-lgtm <aryaman5372@gmail.com>
2025-10-09 18:13:08 +0530

    day end

--- src/card.jsx
+++ src/card.jsx
@@ -43,7 +43,7 @@ const Card = () => {
 					className="filter-dropdown"
 					value={selectedRegion}
 					onChange={(e) => setSelectedRegion(e.target.value)}>
-					<option value="">Filter by Region</option>
+					<option value=""> Filter by Region</option>
 					<option value="Africa">Africa</option>
 					<option value="Americas">America</option>
 					<option value="Asia">Asia</option>
